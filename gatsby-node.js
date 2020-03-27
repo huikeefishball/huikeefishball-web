94e6a6e38ff414690c9226f39e12060abe851405
fishball Hui Kee <huikeefishball@gmail.com>
2020-03-27 14:38:17 +0000

    🌐 Add localization strings for products page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+const fs = require("fs")
+const path = require(`path`)
+const yaml = require("js-yaml")
+
 const createHomePages = require('./src/page-generator/home')
 const createProductsPages = require('./src/page-generator/products')
 const createShopsPages = require('./src/page-generator/shops')
@@ -5,8 +9,9 @@ const createContactPages = require('./src/page-generator/contact')
 
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
+  const productPageData = yaml.safeLoad(fs.readFileSync(path.resolve("data/products_page.yml"), "utf-8"))
   await createHomePages(createPage, graphql)
-  await createProductsPages(createPage, graphql)
+  await createProductsPages(createPage, graphql, productPageData)
   await createShopsPages(createPage, graphql)
   await createContactPages(createPage, graphql)
 }
