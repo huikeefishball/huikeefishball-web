6003b9df154cf29c558b1359af3c17b902c74a52
fishball Hui Kee <huikeefishball@gmail.com>
2020-03-27 14:38:17 +0000

    🌐 Add localization strings for products page

--- src/page-generator/products.js
+++ src/page-generator/products.js
@@ -3,7 +3,10 @@ const path = require(`path`)
 const { languages } = require('../locales')
 const { localizeURL } = require('../utils/localization')
 
-module.exports = async (createPage, graphql) => {
+module.exports = async (createPage, graphql, pageData) => {
+  const pagePath = "/products"
+  const { list_items } = pageData
+  const productIdsList = list_items.map(({ id }) => id)
   const result = await graphql(`
     {
       allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/products/" } }) {
@@ -36,29 +39,42 @@ module.exports = async (createPage, graphql) => {
   languages.forEach(({ 
     code: language,
   }) =>{
-    const pagePath = "/products"
+    const {
+      title: { [language]: pageTitle },
+      description: { [language]: pageDesciption },
+      quote: {
+        content: { [language]: quoteContent },
+        author: { [language]: quoteAuthor },
+      },
+    } = pageData
+    const products = result.data.allMarkdownRemark.edges.reduce((acc, edge) => {
+      const {
+        id,
+        title: { [language]: title },
+        description: { [language]: description },
+        bg_image: bgImage,
+        bg_title: { [language]: bgTitle },
+      } = edge.node.frontmatter
+      acc[id] = {
+        id,
+        title,
+        description,
+        bgImage,
+        bgTitle,
+      }
+      return acc
+    }, {})
     createPage({
       path: localizeURL(language, pagePath),
       component: path.resolve(`src/templates/products.js`),
       context: {
         language,
         pagePath,
-        products: result.data.allMarkdownRemark.edges.map(edge => {
-          const {
-            id,
-            title: { [language]: title },
-            description: { [language]: description },
-            bg_image: bgImage,
-            bg_title: { [language]: bgTitle },
-          } = edge.node.frontmatter
-          return {
-            id,
-            title,
-            description,
-            bgImage,
-            bgTitle,
-          }
-        }),
+        pageTitle,
+        pageDesciption,
+        quoteContent,
+        quoteAuthor,
+        products: productIdsList.map(productId => products[productId]),
       },
     })
   })
